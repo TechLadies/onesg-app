ac67b531749983af312f901997c85e35d609f27d
Fara Aileen T. Blanco <fara.blanco@gmail.com>
2020-10-03 12:22:28 +0800

    refactor: change server setup to use built-in https and http modules
    updates:
    * define http and https servers
    * use configuration file to retrieve hostname and https options
    
    **RELATES TO:** ON-33

--- index.js
+++ index.js
@@ -9,6 +9,11 @@
 /**
  * Module dependencies.
  */
+// Built-in modules
+const http = require('http');
+const https = require('https');
+
+// Third-party modules
 const express = require('express');
 const cors = require('cors');
 const bodyparser = require('body-parser');
@@ -22,9 +27,17 @@ const homeRouter = require('./routers');
 const userRouter = require('./routers/users');
 const healthRouter = require('./routers/healthcheck');
 
+/**
+ * Express server set up
+ */
 const app = express();
+const server = SERVER_CONFIG.USE_HTTPS
+  ? https.createServer(SERVER_CONFIG.HTTPS_OPTIONS, app)
+  : http.createServer(app);
 
-/* cors, bodyparser and morgan */
+// TODO: add handler to force HTTPS redirects for unsecure connection requests if USE_HTTPS is true
+
+// CORS
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // should limit to onesg-web
   res.header('Access-Control-Allow-Headers', '*');
@@ -34,8 +47,13 @@ app.use((req, res, next) => {
   }
   next();
 });
+
+
+// Body Parser
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
+
+// Logger
 app.use(morgan('dev'));
 
 /* 
@@ -51,8 +69,12 @@ app.use('/', homeRouter);
 app.use('/api/users', userRouter);
 app.use('/api/healthcheck', healthRouter);
 
-app.listen(SERVER_CONFIG.PORT, () => {
+
+/**
+ * Start listening to connection requests made on specified PORT
+ */
+server.listen(SERVER_CONFIG.PORT, () => {
   console.log(
-    `Example app listening at http://localhost:${SERVER_CONFIG.PORT}`
+    `OneSG API SErver listening at http://${SERVER_CONFIG.HOSTNAME}:${SERVER_CONFIG.PORT}`
   );
 });
