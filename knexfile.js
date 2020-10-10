4caacf58abd9d0ff825bf3e5ad066221de348878
herzizza88 <herzizza88@gmail.com>
2020-10-10 11:48:33 +0800

    updated eslintrc and knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,3 @@
-// Update with your config settings.
-
 module.exports = {
   development: {
     client: 'sqlite3',
@@ -7,39 +5,5 @@ module.exports = {
       filename: 'database/one-sg.db',
     },
     useNullAsDefault: true,
-  }
-
-
-//   staging: {
-//     client: 'postgresql',
-//     connection: {
-//       database: 'my_db',
-//       user:     'username',
-//       password: 'password'
-//     },
-//     pool: {
-//       min: 2,
-//       max: 10
-//     },
-//     migrations: {
-//       tableName: 'knex_migrations'
-//     }
-//   },
-
-//   production: {
-//     client: 'postgresql',
-//     connection: {
-//       database: 'my_db',
-//       user:     'username',
-//       password: 'password'
-//     },
-//     pool: {
-//       min: 2,
-//       max: 10
-//     },
-//     migrations: {
-//       tableName: 'knex_migrations'
-//     }
-//   }
-
- };
+  },
+}
