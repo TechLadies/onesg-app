0b95c50a73166175986b89e2b01308c495ad6b21
Tong <tong@Tongs-MacBook-Air.local>
2020-10-27 18:52:26 +0800

    chore: estbalish db connection from config > env
    **RELATES TO**: ON-44

--- src/controllers/routers/referees.js
+++ src/controllers/routers/referees.js
@@ -12,10 +12,7 @@ const {
   errors: { BadRequest, UnprocessableEntity },
 } = require('../../utils');
 
-const environment = process.env.NODE_ENV || 'development';
-const configuration = require('../../../knexfile')[environment];
-// eslint-disable-next-line import/order
-const db = require('knex')(configuration);
+const db = require('../../../config/db');
 
 /**
  * Retrieve all references
