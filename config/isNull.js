121a932ef23df3d9f1465b9caee8352957f52ee6
herzizza88 <herzizza88@gmail.com>
2020-11-03 13:29:02 +0800

    add format for ben id

--- config/isNull.js
+++ config/isNull.js
@@ -2,7 +2,6 @@
 const isNull = (req, res, next) => {
   if (req.body.Email === '') {
     req.body.Email = null;
-    console.log(typeof req.body.Email);
   }
   if (req.body.Phone === '') {
     req.body.Phone = null;
