e5831c5cd6de9be7403bf22675b8775381e66112
herzizza88 <herzizza88@gmail.com>
2020-11-25 22:28:07 +0800

    added auth error

--- src/controllers/routers/auth.js
+++ src/controllers/routers/auth.js
@@ -2,17 +2,19 @@
 const { issueJWT } = require('../../utils/auth/passport');
 const { isValidateAdminUser } = require('../../helpers/auth/admin');
 
+const {
+  errors: { ResourceNotFound },
+} = require('../../utils');
+
 // Validate an existing user and issue a JWT
-const login = (req, res) => {
+const login = (req, res, next) => {
   const { email, password } = req.body;
   // returns a boolean that is true and therefore is logged in.
   const hasValidCredentials = isValidateAdminUser(email, password);
 
   // Return 404, if admin user not found !hasValidCredential means false boolean
   if (!hasValidCredentials) {
-    return res
-      .status(404)
-      .json({ message: 'No user found. Please contact admin.' });
+    return next(new ResourceNotFound(`Admin user not found`));
   }
 
   // Only embed email in jwt payload
