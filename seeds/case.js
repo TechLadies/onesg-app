21fc1f2da00b945f46ce0264021059f3001391a9
Fara Aileen T. Blanco <fara.blanco@gmail.com>
2020-11-27 00:24:53 +0800

    fix: update previous fix to promote update function in seed file

--- seeds/case.js
+++ seeds/case.js
@@ -6,44 +6,43 @@ exports.seed = function seedCaseTable(knex) {
     .del()
     .then(function insertCases() {
       // Inserts seed entries
-      return knex(tableCase)
-        .insert([
-          {
-            caseId: 'EF2020-11001',
-            caseStatus: 'NEW',
-            appliedOn: '2020-11-01', // YYYY-MM-DD
-            pointOfContact: '',
-            referenceStatus: 'UNVERIFIED',
-            amountRequested: '350.55',
-            amountGranted: '350',
-            refereeId: 'R2020-11001',
-            beneficiaryId: 'B2020-11002',
-            createdBy: '1',
-            updatedBy: '1',
-          },
-          {
-            caseId: 'EF2020-11002',
-            caseStatus: 'PENDING',
-            pointOfContact: 'John Lim',
-            referenceStatus: 'PENDING',
-            casePendingReason: 'Beneficiary uncontactable',
-            amountRequested: '175.50',
-            refereeId: 'R2020-11002',
-            beneficiaryId: 'B2020-11003',
-            createdBy: '2',
-            updatedBy: '2',
-          },
-        ])
-        .then(function updateCaseEF202011001() {
-          return knex
-            .table(tableCase)
-            .where({ caseId: 'EF2020-11001' })
-            .update({
-              documents: JSON.stringify([
-                { title: 'Application form', url: 'http://www.link.com' },
-                { title: 'Receipt', url: 'http://www.receipt.com' },
-              ]),
-            });
+      return knex(tableCase).insert([
+        {
+          caseId: 'EF2020-11001',
+          caseStatus: 'NEW',
+          appliedOn: '2020-11-01', // YYYY-MM-DD
+          pointOfContact: '',
+          referenceStatus: 'UNVERIFIED',
+          amountRequested: '350.55',
+          amountGranted: '350',
+          refereeId: 'R2020-11001',
+          beneficiaryId: 'B2020-11002',
+          createdBy: '1',
+          updatedBy: '1',
+        },
+        {
+          caseId: 'EF2020-11002',
+          caseStatus: 'PENDING',
+          pointOfContact: 'John Lim',
+          referenceStatus: 'PENDING',
+          casePendingReason: 'Beneficiary uncontactable',
+          amountRequested: '175.50',
+          refereeId: 'R2020-11002',
+          beneficiaryId: 'B2020-11003',
+          createdBy: '2',
+          updatedBy: '2',
+        },
+      ]);
+    })
+    .then(function updateCaseEF202011001() {
+      return knex
+        .table(tableCase)
+        .where({ caseId: 'EF2020-11001' })
+        .update({
+          documents: JSON.stringify([
+            { title: 'Application form', url: 'http://www.link.com' },
+            { title: 'Receipt', url: 'http://www.receipt.com' },
+          ]),
         });
     });
 };
