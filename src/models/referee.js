5617352635653e0f34ec53b2aa5c690b1e5b8ea5
Tong <tong@Tongs-MacBook-Air.local>
2020-12-08 00:30:38 +0800

    update refereeNumber helper function

--- src/models/referee.js
+++ src/models/referee.js
@@ -12,7 +12,7 @@ function getRefereeNumber(previousNumber) {
     year,
     month,
     index,
-  ] = previousNumber.match(/EF(\d{4})-(\d{2})(\d{3})/);
+  ] = previousNumber.match(/R(\d{4})-(\d{2})(\d{3})/);
 
   const today = new Date();
   const currentMonth = (today.getMonth() + 1).toString().padStart(2, '0');
