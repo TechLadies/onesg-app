86a2eb9f4b0c194e6468791c97dbe4b8e520612a
Tong <tong@Tongs-MacBook-Air.local>
2020-12-12 00:48:56 +0800

    remove unrelated parts to ON-66 from beneficiary and case models

--- src/models/case.js
+++ src/models/case.js
@@ -1,38 +1,10 @@
-const { ValidationError, Model } = require('objection');
-const { Request } = require('./request');
+const { Model } = require('objection');
 
 const caseStatusEnum = ['NEW', 'PENDING', 'REFERRED', 'PROCESSING', 'CLOSED'];
 
 const referenceStatusEnum = ['UNVERIFIED', 'PENDING', 'VERIFIED'];
 
 const tableCase = 'case';
-
-// helper functions
-function getCaseNumber(previousNumber) {
-  const [
-    // eslint-disable-next-line no-unused-vars
-    _,
-    year,
-    month,
-    index,
-  ] = previousNumber.match(/EF(\d{4})-(\d{2})(\d{3})/);
-
-  const today = new Date();
-  const currentMonth = (today.getMonth() + 1).toString().padStart(2, '0');
-  const currentYear = today.getFullYear().toString();
-
-  let caseIndex = 1;
-  // If last inserted beneficiary is from the current month,
-  // use counter from previous insert
-  if (year === currentYear && month === currentMonth) {
-    caseIndex = parseInt(index, 10) + 1;
-  }
-
-  // add leading 0s
-  const paddedIndex = String(caseIndex).padStart(3, '0');
-
-  return `EF${currentYear}-${currentMonth}${paddedIndex}`;
-}
 
 class Case extends Model {
   static get tableName() {
@@ -70,43 +42,6 @@ class Case extends Model {
         updatedBy: { type: 'integer' },
       },
     };
-  }
-
-  static get relationMappings() {
-    return {
-      requests: {
-        relation: Model.HasManyRelation,
-        modelClass: Request,
-        join: {
-          from: 'case.id',
-          to: 'request.caseId',
-        },
-      },
-    };
-  }
-
-  async $beforeInsert() {
-    const lastInsertedCase = await Case.query()
-      .select('caseNumber')
-      .orderBy('caseNumber', 'desc')
-      .orderBy('createdAt', 'desc')
-      .limit(1);
-
-    this.caseNumber = getCaseNumber(lastInsertedCase[0].caseNumber);
-  }
-
-  $afterValidate(json) {
-    super.$afterValidate(json);
-    const cases = json;
-
-    // if referenceStatus is pending, casePendingReason cannot be empty
-    if (cases.referenceStatus === 'PENDING') {
-      if (cases.casePendingReason === '' || cases.casePendingReason === null) {
-        throw new ValidationError({
-          message: 'Case pending reason required',
-        });
-      }
-    }
   }
 }
 
