4dbaa7a57b8d67765aecb5c14e6aacbd987c3b28
Tong <tong@Tongs-MacBook-Air.local>
2020-12-30 00:02:39 +0800

    feat: create query param for staff status (active and disabled)
    **RELATES TO**: ON-84

--- src/controllers/routers/staffs.js
+++ src/controllers/routers/staffs.js
@@ -6,6 +6,9 @@
 
 'use strict';
 
+const url = require('url');
+const querystring = require('querystring');
+
 const { Staff } = require('../../models');
 
 /**
@@ -14,6 +17,20 @@ const { Staff } = require('../../models');
  * @param {Response} res
  */
 const getAll = async (req, res) => {
+  // to obtain the full url
+  const fullUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
+  // to parse the full url to retrieve all query params
+  const parsedUrl = url.parse(fullUrl);
+  // to break down into individual query params
+  const parsedQueries = querystring.parse(parsedUrl.query);
+  if (parsedQueries.status === 'active') {
+    const results = await Staff.query().where('status', 'ACTIVE');
+    return res.status(200).json({ results });
+  }
+  if (parsedQueries.status === 'disabled') {
+    const results = await Staff.query().where('status', 'DISABLED');
+    return res.status(200).json({ results });
+  }
   const results = await Staff.query();
   return res.status(200).json({ results });
 };
