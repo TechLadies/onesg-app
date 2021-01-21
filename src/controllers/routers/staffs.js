36750f72b13d3101295e94879d957d3f076ae39f
Tong <tong@Tongs-MacBook-Air.local>
2021-01-21 11:32:53 +0800

    feat: remove url and querystring and update status validation
    if status is not active or disabled, throw error
    use req.query
    
    **RELATES TO**: ON-84

--- src/controllers/routers/staffs.js
+++ src/controllers/routers/staffs.js
@@ -6,33 +6,43 @@
 
 'use strict';
 
-const url = require('url');
-const querystring = require('querystring');
-
 const { Staff } = require('../../models');
+
+const {
+  errors: { InvalidInput },
+} = require('../../utils');
 
 /**
  * Retrieve all referees
  * @param {Request} req
  * @param {Response} res
  */
-const getAll = async (req, res) => {
-  // to obtain the full url
-  const fullUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
-  // to parse the full url to retrieve all query params
-  const parsedUrl = url.parse(fullUrl);
-  // to break down into individual query params
-  const parsedQueries = querystring.parse(parsedUrl.query);
-  if (parsedQueries.status === 'active') {
-    const results = await Staff.query().where('status', 'ACTIVE');
+const getAll = async (req, res, next) => {
+  const parsedQueries = req.query;
+  try {
+    if (
+      parsedQueries.status !== 'active' &&
+      parsedQueries.status !== 'disabled'
+    ) {
+      return next(
+        new InvalidInput(
+          `Status can only be 'active' or 'disabled', ${parsedQueries.status} is invalid, `
+        )
+      );
+    }
+    if (parsedQueries.status === 'active') {
+      const results = await Staff.query().where('status', 'ACTIVE');
+      return res.status(200).json({ results });
+    }
+    if (parsedQueries.status === 'disabled') {
+      const results = await Staff.query().where('status', 'DISABLED');
+      return res.status(200).json({ results });
+    }
+    const results = await Staff.query();
     return res.status(200).json({ results });
+  } catch (err) {
+    return next();
   }
-  if (parsedQueries.status === 'disabled') {
-    const results = await Staff.query().where('status', 'DISABLED');
-    return res.status(200).json({ results });
-  }
-  const results = await Staff.query();
-  return res.status(200).json({ results });
 };
 
 module.exports = {
