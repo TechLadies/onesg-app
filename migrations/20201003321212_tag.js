a3ad06d4deb4a4d534a8fd27f906f674fdc65a37
Sam Lee <leeway95@gmail.com>
2021-03-13 13:28:49 +0100

    Make tag non nullable

--- migrations/20201003321212_tag.js
+++ migrations/20201003321212_tag.js
@@ -3,7 +3,7 @@ const { tableTag } = require('../src/models/tag.js');
 exports.up = function makeTagTable(knex) {
   return knex.schema.createTable(tableTag, (table) => {
     table.increments('id').primary().index();
-    table.string('name', 25).unique().comment('Name of tag');
+    table.string('name', 25).notNullable().unique().comment('Name of tag');
   });
 };
 
