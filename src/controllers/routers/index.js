bab22e9e26bfee653c10ce28d9fec26894b93151
Sam Lee <leeway95@gmail.com>
2021-03-13 14:08:18 +0100

    Add endpoints for getting and creating tags

--- src/controllers/routers/index.js
+++ src/controllers/routers/index.js
@@ -13,6 +13,7 @@ const staffs = require('./staffs');
 const requestTypes = require('./requestTypes');
 const search = require('./search');
 const comments = require('./comments');
+const tags = require('./tags');
 
 module.exports = {
   beneficiaries,
@@ -22,4 +23,5 @@ module.exports = {
   requestTypes,
   search,
   comments,
+  tags,
 };
