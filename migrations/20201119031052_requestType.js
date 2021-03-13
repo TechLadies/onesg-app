25540e6c0f1c82b25eff3b0acf1fa44d66056bbd
Sam Lee <leeway95@gmail.com>
2021-03-13 14:35:08 +0100

    revert changes to migration scripts

--- migrations/20201119031052_requestType.js
+++ migrations/20201119031052_requestType.js
@@ -3,11 +3,7 @@ const { tableRequestType } = require('../src/models/requestType.js');
 exports.up = function makeRequestTypeTable(knex) {
   return knex.schema.createTable(tableRequestType, (table) => {
     table.increments('id').primary().index();
-    table
-      .string('type', 50)
-      .notNullable()
-      .unique()
-      .comment('Name of request type');
+    table.string('type', 50).unique().comment('Name of request type');
   });
 };
 
